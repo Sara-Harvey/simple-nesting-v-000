de3fc7ef1aa616b823eca9f67228d10cf3791ded
Sara Harvey <saraharveynyc@gmail.com>
2018-10-24 20:16:12 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -16,8 +16,9 @@ def hopper
         }
      }
 
-
+programmer_hash[:grace_hopper]
 end
+
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
@@ -37,6 +38,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +57,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+programmer_hash[:dennis_ritchie][:languages].join
 end
 
 def adding_matz
@@ -80,8 +83,13 @@ def adding_matz
         }
      }
 
-    
+programmer_hash[:yukihiro_matsumoto] = { :known_for => "Ruby", :languages => ["LISP", "C"]}}
+programmer_hash.merge(:yukihiro_matsumoto){|key,newval, oldval|
 end
+
+
+
+
 
 def changing_alan
 	programmer_hash = 
